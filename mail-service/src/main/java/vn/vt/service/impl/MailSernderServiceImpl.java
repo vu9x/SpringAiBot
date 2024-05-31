9bb92e31fe7b5366c43ee7f6f30fc2f182e52178
Vu <nguyentuanvu9x@gmail.com>
2024-05-31 16:39:38 +0300

    modified: mail-service, adding yandex.smtp protocol and deleting googleAuth

--- mail-service/src/main/java/vn/vt/service/impl/MailSernderServiceImpl.java
+++ mail-service/src/main/java/vn/vt/service/impl/MailSernderServiceImpl.java
@@ -1,44 +1,43 @@
-//package vn.vt.service.impl;
-//
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.mail.SimpleMailMessage;
-//import org.springframework.mail.javamail.JavaMailSender;
-//import org.springframework.stereotype.Service;
-//import vn.vt.dto.MailParams;
-//import vn.vt.service.MailSenderService;
-//
-//@Service
-//public class MailSernderServiceImpl implements MailSenderService {
-//    private final JavaMailSender javaMailSender;
-//    @Value("${spring.mail.username}")
-//    private String emailFrom;
-//    @Value("${service.activation.uri}")
-//    private String activationServiceUri;
-//
-//    public MailSernderServiceImpl(JavaMailSender javaMailSender) {
-//        this.javaMailSender = javaMailSender;
-//    }
-//
-//
-//    @Override
-//    public void send(MailParams mailParams) {
-//        var subject = "Активация учетной записи";
-//        String messageBody = getActivationMailBody(mailParams.getId());
-//        var emailTo = mailParams.getEmailTo();
-//
-//        SimpleMailMessage mailMessage = new SimpleMailMessage();
-//        mailMessage.setFrom(emailFrom);
-//        mailMessage.setTo(emailTo);
-//        mailMessage.setSubject(subject);
-//        mailMessage.setText(messageBody);
-//
-//        javaMailSender.send(mailMessage);
-//    }
-//
-//
-//    private String getActivationMailBody(String id) {
-//        var msg = String.format("Для завершения регистрации перейдите по ссылке:\n%s",
-//                activationServiceUri);
-//        return msg.replace("{id}", id);
-//    }
-//}
+package vn.vt.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+import vn.vt.dto.MailParams;
+import vn.vt.service.MailSenderService;
+
+@Log4j2
+@RequiredArgsConstructor
+@Service
+public class MailSernderServiceImpl implements MailSenderService {
+    private final JavaMailSender javaMailSender;
+    @Value("${spring.mail.username}")
+    private String emailFrom;
+    @Value("${service.activation.uri}")
+    private String activationServiceUri;
+
+    @Override
+    public void send(MailParams mailParams) {
+        var subject = "Активация учетной записи";
+        String messageBody = getActivationMailBody(mailParams.getId());
+        var emailTo = mailParams.getEmailTo();
+
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setFrom(emailFrom);
+        mailMessage.setTo(emailTo);
+        mailMessage.setSubject(subject);
+        mailMessage.setText(messageBody);
+
+        javaMailSender.send(mailMessage);
+    }
+
+
+    private String getActivationMailBody(String id) {
+        var msg = String.format("Для завершения регистрации перейдите по ссылке:\n%s",
+                activationServiceUri);
+        return msg.replace("{id}", id);
+    }
+}
