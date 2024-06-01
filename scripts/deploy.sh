89c987c448d239cecdc8c1d74d8147f09b9e4982
Vu <nguyentuanvu9x@gmail.com>
2024-06-01 20:06:58 +0300

    adding: deploy.sh file

---
+++ scripts/deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+ENV_FILE="./.env"
+
+# Обновление кода и деплой backend приложения
+pushd ~/springAiBot/ || exit
+
+# Обновляем ветку main
+git pull
+
+# Останавливаем старые контейнеры микросервисов и запускаем новые, с обновлённым кодом
+docker compose -f docker-compose.yml --env-file $ENV_FILE down --timeout=60 --remove-orphans
+docker compose -f docker-compose.yml --env-file $ENV_FILE up --build --detach
+
+popd || exit
