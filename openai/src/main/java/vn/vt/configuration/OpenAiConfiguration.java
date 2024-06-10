abe15457c485568f6a243c3730ad006515106d18
Vu <nguyentuanvu9x@gmail.com>
2024-06-10 17:47:11 +0300

    modified: adding CryptoTool and salt into openai service

---
+++ openai/src/main/java/vn/vt/configuration/OpenAiConfiguration.java
@@ -0,0 +1,18 @@
+package vn.vt.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import vn.vt.utils.CryptoTool;
+
+@Configuration
+public class OpenAiConfiguration {
+
+    @Value("${salt}")
+    private String salt;
+
+    @Bean
+    public CryptoTool getCryptoTool(){
+        return new CryptoTool(salt);
+    }
+}
