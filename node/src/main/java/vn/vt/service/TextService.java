6633e7db474d094f6a50ad17962db5822f2fc31f
Vu <nguyentuanvu9x@gmail.com>
2024-06-10 23:45:03 +0300

    modified: dispatcher. Deleting unused dependency

--- node/src/main/java/vn/vt/service/TextService.java
+++
@@ -1,9 +0,0 @@
-//package vn.vt.service;
-//
-//import org.telegram.telegrambots.meta.api.objects.Update;
-//import vn.vt.entity.AppOpenAiResponses;
-//import vn.vt.entity.AppUser;
-//
-//public interface TextService {
-//    AppOpenAiResponses processText(Update update, AppUser appUser, String openAiResponse);
-//}
